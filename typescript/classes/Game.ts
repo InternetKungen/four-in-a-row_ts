1940c38940f9db25b31c7165921965e28d536d2e
Kim Andersson <kandersson@gmail.com>
2024-08-26 20:23:55 +0200

    A simple start menu added

--- typescript/classes/Game.ts
+++ typescript/classes/Game.ts
@@ -8,19 +8,61 @@ export class Game {
   player2: Player;
   board: Board;
 
+
   constructor() {
 
+    //Start Menu
+    this.startMenu();
     this.player1 = new Player(prompt('Name of Player 1: '), 'X');
     this.player2 = new Player(prompt('Name of Player 2: '), 'O');
     this.board = new Board();
 
   }
 
+  startMenu(): void {
+    console.clear();
+    console.log("=== Four in a row ===");
+    console.log("1. Player vs. Player");
+    console.log("2. Player vs. Computer");
+    console.log("3. Exit Game");
+
+    const choice = prompt("Enter your choice (1-3): ");
+
+      switch (choice) {
+        case '1':
+          this.startPlayerVsPlayer();
+          break;
+        case '2':
+          this.startPlayerVsComputer();
+          break;
+        case '3':
+          console.log("Exiting game...");
+          break;
+        default:
+          console.log("Invalid choice. Try again...");
+          this.startMenu();
+          break;
+      }
+    }
+  
+  startPlayerVsPlayer(): void {
+    this.player1 = new Player(prompt('Name of Player 1: '), 'X');
+    this.player2 = new Player(prompt('Name of Player 2: '), 'O');
+    this.board = new Board();
+    this.start();
+  }
+  
+  startPlayerVsComputer(): void {
+    this.player1 = new Player(prompt('Name of Player 1: '), 'X');
+    this.player2 = new Player("Computer", 'O');
+    this.board = new Board();
+    this.start();
+  }
+  
   start(): void {
     console.clear();
-    console.log(`Hej ${this.player1} och ${this.player2}!`);
     this.board.render();
-    this.gameLoop();  // Kör spel-loopen
+    this.gameLoop();
   }
 
   gameLoop(): void {
@@ -37,7 +79,7 @@ export class Game {
           //board colums are 1,2,3,4,5,6,7, but also albe to be dynamic.
           // remove 1 from column number to match array index
           let column: number = parseInt(prompt(
-              `Make your move ${player.color} ${player.name} - input column number (1-${this.board.columns}): `)) - 1;
+              `Make your move ( ${player.color} ) ${player.name} - input column number (1-${this.board.columns}): `)) - 1;
             
           // try to make the move
           //send current player color and column number
